4094378fb7385679fb4ad2ca43c5bb909ca09c53
mkireva <mariya@kireva.de>
2023-01-24 15:30:49 +0100

    migrate to pnpm and install osmd

---
+++ app/lib/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import { getRandomId, iife } from "@milkscout/react";
+import { OpenSheetMusicDisplay as OSMD } from "opensheetmusicdisplay";
+import { IOSMDOptions } from "opensheetmusicdisplay/build/dist/src/OpenSheetMusicDisplay/OSMDOptions";
+
+export interface CmpProps {
+  id: string;
+}
+
+const DEFAULT_OSMD_OPTIONS: IOSMDOptions = {
+  autoResize: true,
+};
+
+export interface OsmdProps {
+  options?: IOSMDOptions;
+  file: string | Document;
+}
+
+const Osmd = ({ options = DEFAULT_OSMD_OPTIONS, file }: OsmdProps) => {
+  const [id] = useState(getRandomId());
+  const renderRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    iife(async () => {
+      const { current: handleRenderRef } = renderRef;
+      if (!handleRenderRef) {
+        // reference missing
+        return;
+      }
+      //reset rendering
+      handleRenderRef.innerHTML = "";
+      const osm = new OSMD(id, options);
+      await osm.load(file);
+      osm.render();
+    });
+  });
+  return <div ref={renderRef} id={id} />;
+};
+
+export default Osmd;
--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
     ],
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.xml$/i,
+      loader: "raw-loader",
+    });
+
+    // Important: return the modified config
+    return config;
+  },
 };
 
 module.exports = nextConfig;
